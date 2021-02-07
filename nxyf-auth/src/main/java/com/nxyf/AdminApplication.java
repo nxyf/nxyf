73e2335165438df27302a88af7138a5b772d65b9
niutian <123@163.com>
2021-02-07 11:11:33 +0800

    添加cache

--- nxyf-auth/src/main/java/com/nxyf/AdminApplication.java
+++ nxyf-auth/src/main/java/com/nxyf/AdminApplication.java
@@ -3,6 +3,7 @@ package com.nxyf;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
 @MapperScan(basePackages = {"com.nxyf.modules.system.mapper"})
+@EnableCaching //开启缓存功能
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
